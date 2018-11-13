c0949c0703ae931cf5267bc7e32a304a6bb2731d
kjirou <sorenariblog@gmail.com>
2018-11-13 14:42:09 +0900

    Members can know the attack target team now

--- lib/poodr_rpg/member.rb
+++ lib/poodr_rpg/member.rb
@@ -11,13 +11,21 @@ module PoodrRpg
     end
 
     def do_action(all_parties)
-      party = find_my_party(all_parties)
+      my_party = find_my_party(all_parties)
 
-      unless party
+      unless my_party
         raise 'A member can not do actions if it dose not belong to a party.'
       end
 
-      "#{name}(#{party.name}) did an action."
+      rest_parties = all_parties.reject {|party| party == my_party}
+
+      if rest_parties.size != 1
+        raise 'Currently, there is only one team to be targeted for attack.'
+      end
+
+      attack_target_party = rest_parties[0]
+
+      "#{name}(#{my_party.name}) did an action. Attacked to \"#{attack_target_party.name}\" party!"
     end
 
     private
