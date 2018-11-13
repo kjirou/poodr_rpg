cbf144b2412bf23bcddbe8192add912c09751c9a
kjirou <sorenariblog@gmail.com>
2018-11-13 11:28:19 +0900

    Make that one member acts for each turn

--- lib/poodr_rpg/battle.rb
+++ lib/poodr_rpg/battle.rb
@@ -5,6 +5,7 @@ module PoodrRpg
     def initialize(parties:)
       @parties = parties
       @turn_count = 0
+      @logs_for_each_turn = {}
     end
 
     def winner
@@ -15,13 +16,31 @@ module PoodrRpg
 
     def transit_to_next_turn
       @turn_count += 1
-      # do stuff
+
+      member_to_act = choice_next_member_to_act
+      @logs_for_each_turn[@turn_count] = "#{member_to_act.name} acted."
     end
 
     def to_text
       text = "Turns: #{@turn_count}"
+      text += "\n#{@logs_for_each_turn[@turn_count]}"
       text += "\n#{winner} won!" if winner
       text
+    end
+
+    private
+
+    def choice_next_member_to_act
+      all_members.sample
+    end
+
+    def all_members
+      # Q: Write it shorter?
+      all_members = []
+      @parties.each do |party|
+        all_members.concat(party.members)
+      end
+      all_members
     end
   end
 end
